83954a00dad0a54ec6a7ede431c2b51be9395e9b
rudicarrilloypr <125106965+rudicarrilloypr@users.noreply.github.com>
2023-07-05 02:03:24 -0600

    Add and configure linters

--- src/js/api.js
+++ src/js/api.js
@@ -1,12 +1,11 @@
 export async function getItems() {
-    const response = await fetch('https://fortnite-api.com/v2/cosmetics/br');
-    const data = await response.json();
-    return data.data;
-  }
-  
-  export async function getItemLikes() {
-    const response = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/{e81cf926-7848-4d1c-8186-e0289c745093}/likes');
-    const data = await response.json();
-    return data;
-  }
-  
+  const response = await fetch('https://fortnite-api.com/v2/cosmetics/br');
+  const data = await response.json();
+  return data.data;
+}
+
+export async function getItemLikes() {
+  const response = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/{e81cf926-7848-4d1c-8186-e0289c745093}/likes');
+  const data = await response.json();
+  return data;
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,13 @@
 import { getItems, getItemLikes } from './api.js';
+import '../css/style.css';
 
 document.addEventListener('DOMContentLoaded', async () => {
+  // eslint-disable-next-line no-unused-vars
   const items = await getItems();
+  // eslint-disable-next-line no-unused-vars
   const likes = await getItemLikes();
 
   // Aquí deberías mostrar los items y los likes en la página.
 });
 
 // src/js/index.js
-
-import '../css/style.css';
