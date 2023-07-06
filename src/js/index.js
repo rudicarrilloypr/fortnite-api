a25188d51528bb8b95715395836968ed43a265be
rudicarrilloypr <125106965+rudicarrilloypr@users.noreply.github.com>
2023-07-05 21:31:03 -0600

    Add cosmetics and style

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,30 @@
-import { getItems, getItemLikes } from './api.js';
+import { getItems/* , getItemLikes */ } from './api.js';
 import '../css/style.css';
 
 document.addEventListener('DOMContentLoaded', async () => {
-  // eslint-disable-next-line no-unused-vars
   const items = await getItems();
+  // const likes = await getItemLikes();
+
+  const itemsContainer = document.querySelector('#items');
+
+  items.forEach((item) => {
+    const itemElement = document.createElement('div');
+    itemElement.classList.add('item');
+    itemElement.innerHTML = `
+      <h2>${item.name}</h2>
+      <img src="${item.images.icon}" alt="${item.name}">
+      <p>${item.description}</p>
+      <button>Comments</button>
+    `;
+
+    itemsContainer.appendChild(itemElement);
+  });
+
+  // Aquí puedes mostrar los likes en la página. Dependiendo de cómo quieras
+  // mostrarlos, puede que necesites hacer algo más que simplemente iterar
+  // sobre ellos y añadirlos al DOM.
   // eslint-disable-next-line no-unused-vars
-  const likes = await getItemLikes();
-
-  // Aquí deberías mostrar los items y los likes en la página.
+  // likes.forEach((like) => {
+  // Código para mostrar likes
+  // });
 });
-
-// src/js/index.js
