d68cab459006fcd0d67ead7edff03f67dfe7bbdb
rudicarrilloypr <125106965+rudicarrilloypr@users.noreply.github.com>
2023-07-06 23:04:05 -0600

    Update display cosmetics

--- src/js/api.js
+++ src/js/api.js
@@ -1,11 +1,16 @@
 export async function getItems() {
-  const response = await fetch('https://fortnite-api.com/v2/cosmetics/br');
+  const response = await fetch('https://fortnite-api.com/v2/cosmetics/br/new');
   const data = await response.json();
-  return data.data;
+
+  // Asegúrate de que los datos existen antes de devolverlos
+  if (data && data.data && data.data.items) {
+    return data.data.items;
+  }
+  throw new Error('No se pudieron obtener los items');
 }
 
 export async function getItemLikes() {
-  const response = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/{e81cf926-7848-4d1c-8186-e0289c745093}/likes');
+  const response = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/{4Ra3BPIlZ9RZb5SCWETK}/likes');
   const data = await response.json();
   return data;
 }
