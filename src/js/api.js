61f17d868cd8147c26c83b68c0f3efa1f63d3a39
rudicarrilloypr <125106965+rudicarrilloypr@users.noreply.github.com>
2023-07-08 12:11:10 -0600

    Add items counter

--- src/js/api.js
+++ src/js/api.js
@@ -10,7 +10,53 @@ export async function getItems() {
 }
 
 export async function getItemLikes() {
-  const response = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/{4Ra3BPIlZ9RZb5SCWETK}/likes');
-  const data = await response.json();
-  return data;
+  let likes;
+  try {
+    const response = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/4Ra3BPIlZ9RZb5SCWETK/likes');
+
+    // Check if response is ok
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    // Check if response is not empty
+    if (response.headers.get('content-length') === '0' || response.status === 204) {
+      likes = [];
+    } else {
+      likes = await response.json();
+    }
+  } catch (error) {
+    likes = [];
+  }
+  return likes;
+}
+
+export async function postLike(itemId) {
+  const response = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/4Ra3BPIlZ9RZb5SCWETK/likes', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ item_id: itemId }),
+  });
+
+  if (!response.ok) {
+    const message = `An error has occurred: ${response.status}`;
+    throw new Error(message);
+  }
+}
+
+export async function postDislike(itemId) {
+  const response = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/4Ra3BPIlZ9RZb5SCWETK/dislikes', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ item_id: itemId }),
+  });
+
+  if (!response.ok) {
+    const message = `An error has occurred: ${response.status}`;
+    throw new Error(message);
+  }
 }
--- src/js/index.js
+++ src/js/index.js
@@ -1,30 +1,54 @@
-import { getItems/* , getItemLikes */ } from './api.js';
+import {
+  getItems, getItemLikes, postLike, postDislike,
+} from './api.js';
 import '../css/style.css';
 
 document.addEventListener('DOMContentLoaded', async () => {
   const items = await getItems();
-  // const likes = await getItemLikes();
+  const likes = await getItemLikes();
 
   const itemsContainer = document.querySelector('#items');
+  const itemsCountElement = document.querySelector('#items-count');
+
+  // Actualizar el contador de artículos
+  itemsCountElement.textContent = `(${items.length})`;
 
   items.forEach((item) => {
     const itemElement = document.createElement('div');
     itemElement.classList.add('item');
+
+    const likeCount = likes.find((like) => like.item_id === item.id)?.likes || 0;
+
     itemElement.innerHTML = `
       <h2>${item.name}</h2>
       <img src="${item.images.icon}" alt="${item.name}">
       <p>${item.description}</p>
       <button>Comments</button>
+      <div class="likes">
+        <i class="far fa-thumbs-up like-btn" data-id="${item.id}"></i>
+        <span class="like-count">${likeCount}</span>
+      </div>
     `;
 
     itemsContainer.appendChild(itemElement);
   });
 
-  // Aquí puedes mostrar los likes en la página. Dependiendo de cómo quieras
-  // mostrarlos, puede que necesites hacer algo más que simplemente iterar
-  // sobre ellos y añadirlos al DOM.
-  // eslint-disable-next-line no-unused-vars
-  // likes.forEach((like) => {
-  // Código para mostrar likes
-  // });
+  itemsContainer.addEventListener('click', async (event) => {
+    if (event.target.classList.contains('like-btn')) {
+      const btn = event.target;
+      const itemId = btn.dataset.id;
+      const count = btn.nextElementSibling;
+
+      if (btn.classList.contains('far')) {
+        await postLike(itemId);
+        btn.classList.remove('far');
+        btn.classList.add('fas');
+        count.textContent = Number(count.textContent) + 1;
+      } else {
+        await postDislike(itemId);
+        btn.classList.remove('fas');
+        btn.classList.add('far');
+      }
+    }
+  });
 });
