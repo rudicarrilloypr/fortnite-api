dfa47278ff45c8da4a5f1197a3fdc6270602f5c8
Rodolfo Carrillo <rudicarrilloypr@gmail.com>
2026-06-02 13:12:40 -0600

    Extract production CSS

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = (env, argv) => {
   const isProduction = argv.mode === 'production';
@@ -26,7 +27,7 @@ module.exports = (env, argv) => {
         },
         {
           test: /\.css$/,
-          use: ['style-loader', 'css-loader'],
+          use: [isProduction ? MiniCssExtractPlugin.loader : 'style-loader', 'css-loader'],
         },
         {
           test: /\.html$/,
@@ -40,6 +41,11 @@ module.exports = (env, argv) => {
     },
     plugins: [
       new CleanWebpackPlugin(),
+      ...(isProduction ? [
+        new MiniCssExtractPlugin({
+          filename: '[name].css',
+        }),
+      ] : []),
       new HtmlWebpackPlugin({
         template: './src/html/index.html',
         chunks: ['main'],
